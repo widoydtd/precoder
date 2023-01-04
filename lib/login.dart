d42529f4bc307abdf45649896b2603c6c1a9ca37
widoydtd <widipradhana98@gmail.com>
2023-01-04 11:48:35 +0700

    -Fixed errors in build.gradle -Created a login function in login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:precoder/home.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -16,6 +17,8 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController passwordController = TextEditingController();
 
   final _auth = FirebaseAuth.instance;
+
+  String? errorMessage;
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +75,9 @@ class _LoginPageState extends State<LoginPage> {
       child: MaterialButton(
         padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
         minWidth: MediaQuery.of(context).size.width,
-        onPressed: () {},
+        onPressed: () {
+          signIn(emailController.text, passwordController.text);
+        },
         child: Text("Login", style: TextStyle(color: Colors.white)),
       ),
     );
@@ -94,9 +99,32 @@ class _LoginPageState extends State<LoginPage> {
 
   void signIn(String email, String password) async {
     if (_formKey.currentState!.validate()) {
-      await _auth
-          .signInWithEmailAndPassword(email: email, password: password)
-          .then((uid) => {Fluttertoast.showToast(msg: "Login Successful")});
+      try {
+        await _auth
+            .signInWithEmailAndPassword(email: email, password: password)
+            .then((uid) => {
+                  Fluttertoast.showToast(msg: "Login Successful"),
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => HomePage())),
+                });
+      } on FirebaseAuthException catch (error) {
+        switch (error.code) {
+          case "invalid-email":
+            errorMessage = "The email you entered is incorrect";
+
+            break;
+          case "wrong-password":
+            errorMessage = "The password you entered is incorrect";
+            break;
+          case "user-not-found":
+            errorMessage = "The account doesn't exist";
+            break;
+          default:
+            errorMessage = "An unknown error has occured. Please try again";
+        }
+        Fluttertoast.showToast(msg: errorMessage!);
+        print(error.code);
+      }
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:precoder/login.dart';
 import 'package:precoder/register.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  Firebase.initializeApp();
   runApp(const MyApp());
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Color.fromARGB(100, 0, 0, 0), // status bar color
